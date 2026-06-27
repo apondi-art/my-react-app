829486c63d3db4ad28986ca1a387810170d94142
apondi-art <aiyanarich64@gmail.com>
2026-06-27 20:48:28 +0300

    feat: add Header component

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>Quinter Ochieng</h1>
+      <p>Web Developer — Currently loving frontend!</p>
+    </header>
+  );
+}
+
+export default Header;

cc09a88795703e345e30da0aa38c5b9112df6951
apondi-art <aiyanarich64@gmail.com>
2026-06-27 20:48:47 +0300

    feat: add Hobbies component

---
+++ src/Hobbies.jsx
@@ -0,0 +1,17 @@
+import './Hobbies.css'
+
+function Hobbies() {
+  return (
+    <section className="hobbies-section">
+      <h2>My Hobbies</h2>
+      <ul>
+        <li>Coding</li>
+        <li>Reading</li>
+        <li>Listening to Music</li>
+        <li>Exploring new technologies</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Hobbies;

f6b361ffffbba36b5818cd90632cc6a97c40f231
apondi-art <aiyanarich64@gmail.com>
2026-06-27 20:47:59 +0300

    feat: add About component

---
+++ src/About.jsx
@@ -0,0 +1,16 @@
+import './About.css'
+
+function About() {
+  return (
+    <section className="about-section">
+      <h2>About Me</h2>
+      <p>
+        Hi, I'm Quinter Ochieng, a passionate developer currently enrolled in a web development bootcamp.
+        I enjoy building things for the web and am always eager to learn new technologies.
+        My goal is to become a full-stack developer and create impactful digital experiences.
+      </p>
+    </section>
+  );
+}
+
+export default About;
